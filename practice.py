8b591ae97bf5819145cdeb048c2977ba75ec790d
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-13 21:29:40 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,15 @@
-print('hello')
-print('world')
-print('hello')
+while j > 0:
+  print(i, j)
+  
+  if word[i] != word2[j]:
+     return False
+    
+   i = i + 1
+   j = j - 1
+    
+ 
+def has_no_e(word):
+    for letter in word:
+      if letter == 'e':
+         return False
+    return True
